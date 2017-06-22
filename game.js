61cb882e790578d85a19ef77fd8139dfbd526f22
Usha <usha.macdonald@gmail.com>
2017-06-22 18:54:43 +1200

    added makeBlue function

--- game.js
+++ game.js
@@ -12,6 +12,7 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('click', makeBlue)
   }
 }
 
@@ -22,6 +23,11 @@ function makeGreen (evt) {
 }
 
 // CREATE FUNCTION makeBlue HERE
+
+function makeBlue (evt) {
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
 
 // CREATE FUNCTION hide HERE
 
